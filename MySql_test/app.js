fc9fa5920d9eb20edc7768ad4d56dbc3b5e390f4
Cuitlahuac Rodrigo CRAC. Aristeo Campos <cuitlahuac@snc.com.mx>
2018-03-26 17:29:57 -0600

    app file

---
+++ MySql_test/app.js
@@ -0,0 +1,46 @@
+var connection = require('./modules/sqlconnections.js');
+const express = require('express');
+var ejs = require('ejs');
+const app = express();
+
+const nsconnection = connection.nsconnection;
+
+let queryIdea = 'SELECT Folio, RutaImagen1, fechav3, CantGastada FROM sncideacapproc01 WHERE MID(sncideacapproc01.Folio,4,3) =';
+
+nsconnection.connect(function(error, results, fields){
+    if(error){
+      throw error;
+    }
+    console.log(`mySql Conected ...`);
+});
+
+app.get('/newpost',function(req,res){
+  let qFolio = '001';
+  let query = nsconnection.query((queryIdea + qFolio),function(err, results){
+    if(err) throw err;
+  console.log(results);
+  res.render('mysql.ejs',{sqldos: results})
+  });
+  });
+
+app.post('/idTienda',(req,res) => {
+  res.render('idTienda.ejs');
+});
+
+
+  app.get('/*', (req,res) => {
+    res.send("page not found.\n Página no encontrada.")
+  })
+
+//connection.connect();
+
+/*connection.query('SELECT * FROM sncfases AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});*/
+
+//connection.end();
+
+app.listen('3000', () => {
+  console.log('server conected in port 3000');
+});
